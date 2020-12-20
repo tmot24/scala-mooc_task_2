5ecb300c30f942cb6782fb6ac18f732159dfe814
Tmot <60540588+tmot24@users.noreply.github.com>
2020-12-21 00:52:27 +0300

    2.1

--- src/main/scala/homeworks/collections/task_seq_riddle.scala
+++ src/main/scala/homeworks/collections/task_seq_riddle.scala
@@ -2,7 +2,9 @@ package homeworks.collections
 
 import homeworks.HomeworksUtils.TaskSyntax
 
-object task_seq_riddle {
+import scala.annotation.tailrec
+
+object task_seq_riddle extends App {
 
   /**
    * Рассмотрим последовательность с числами:
@@ -18,8 +20,27 @@ object task_seq_riddle {
    * 1. Реализуйте функцию генерирующую след последовательность из текущей
    * */
 
-  def nextLine(currentLine: List[Int]): List[Int] =
-    task"Реализуйте функцию генерирующую след последовательность из текущей"()
+  def nextLine(currentLine: List[Int]): List[Int] = {
+    @tailrec
+    def nextLine2(currentLine: List[Int], acc: List[List[Int]] = Nil): List[Int] = {
+      currentLine.headOption match {
+        case None =>
+          val list = acc.reverse
+          val size = list.map(_.size)
+          val zip = size.zip(list.flatMap(_.distinct))
+          val result = for {
+            t <- zip
+          } yield List(t._1, t._2)
+          result.flatten
+        case Some(value) =>
+          val firstValues = currentLine.takeWhile(_ == value)
+          val restValues = currentLine.dropWhile(_ == value)
+          nextLine2(restValues, firstValues :: acc)
+      }
+    }
+    nextLine2(currentLine)
+  }
+  println(nextLine(List(1, 1, 1, 2, 2, 1)))
 
   /**
    * 2. Реализуйте ленивый список, который генерирует данную последовательность
@@ -29,6 +50,7 @@ object task_seq_riddle {
    *
    */
 
-  val funSeq: LazyList[List[Int]] =
-    task"Реализуйте ленивый список, который генерирует данную последовательность"()
+
+//  val funSeq: LazyList[List[Int]] = ???
+//    task"Реализуйте ленивый список, который генерирует данную последовательность"()
 }
