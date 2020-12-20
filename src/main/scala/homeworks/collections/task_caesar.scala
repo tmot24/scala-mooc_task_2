349eae28dd2e64c7751ff7480c242846191778dc
Tmot <60540588+tmot24@users.noreply.github.com>
2020-12-20 18:31:17 +0300

    1.1.1

--- src/main/scala/homeworks/collections/task_caesar.scala
+++ src/main/scala/homeworks/collections/task_caesar.scala
@@ -16,18 +16,20 @@ object task_caesar {
    * @return зашифрованное слово
    */
   def encrypt(word: String, offset: Int): String = {
-    val alphabet = 'A' to 'Z'
-    val charArray = word.toCharArray
 
-    val arrayInt = for {
-      c <- charArray
-    } yield (alphabet.indexOf(c) + offset % 26) % 26
+    // Решил двумя разными способами, реализация encrypt мне нравиться больше чем decrypt, что скажите?
+    // на всякий случай вставлю ещё и реализацию decrypt ниже
 
-    val arrayChar = for {
-      i <- arrayInt
-    } yield alphabet(i)
-
-    arrayChar.mkString("")
+    word
+      .toCharArray
+      .map(_.toInt)
+      .map(_ + offset % 26)
+      .collect {
+        case i if i > 90 => i - 26
+        case i => i
+      }
+      .map(_.toChar)
+      .mkString("")
   }
 
 
@@ -37,6 +39,7 @@ object task_caesar {
    * @return расшифрованное слово
    */
   def decrypt(cipher: String, offset: Int): String = {
+
     val alphabet = 'A' to 'Z'
     val charArray = cipher.toCharArray
 
@@ -48,12 +51,22 @@ object task_caesar {
       (26 + alphabet.indexOf(c) - offset % 26) % 26
     }
 
-
     val arrayChar = for {
       i <- arrayInt
     } yield alphabet(i)
 
     arrayChar.mkString("")
+
+    //    word
+    //      .toCharArray
+    //      .map(_.toInt)
+    //      .map(_ - offset % 26)
+    //      .collect {
+    //        case i if i < 65 => i + 26
+    //        case i => i
+    //      }
+    //      .map(_.toChar)
+    //      .mkString("")
   }
 
 }
